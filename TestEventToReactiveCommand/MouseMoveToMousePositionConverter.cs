1ab106c458315f60c2b07f60f8bb4a613d60432b
satorimon <48250071+satorimon@users.noreply.github.com>
2020-06-19 20:16:51 +0900

    https://blog.okazuki.jp/entry/2019/04/19/172010 Prismで動くことを確認

---
+++ TestEventToReactiveCommand/MouseMoveToMousePositionConverter.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TestEventToReactiveCommand
+{
+    public class MouseMoveToMousePositionConverter : ReactiveConverter<MouseEventArgs, MousePosition>
+    {
+        protected override IObservable<MousePosition> OnConvert(IObservable<MouseEventArgs> source) => source
+            .Select(x => x.GetPosition((IInputElement)AssociateObject))
+            .Select(x => new MousePosition
+            {
+                X = x.X,
+                Y = x.Y,
+            });
+    }
+}
---
+++ TestEventToReactiveCommand/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Reactive.Linq;
+
+namespace TestEventToReactiveCommand.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ReactiveCommand<MousePosition> MouseMoveCommand { get; }
+
+        public ReadOnlyReactivePropertySlim<string> Message { get; }
+
+        public MainWindowViewModel()
+        {
+            MouseMoveCommand = new ReactiveCommand<MousePosition>();
+            Message = MouseMoveCommand.Select(x => $"({x.X}, {x.Y})")
+                .ToReadOnlyReactivePropertySlim();
+        }
+    }
+}
